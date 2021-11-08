6dc3d5a16b2ce589df6b97214998a70a5a152147
Your Name <mahmut.osmanovic@outlook.com>
2021-11-09 00:55:08 +0100

    finished menu

--- mahmut.c
+++ mahmut.c
@@ -14,7 +14,7 @@ int rollDice(void)
     int die1 = 1 + (rand() % 6); // pick random die1 value
     int die2 = 1 + (rand() % 6); // pick random die2 value
     // display results of this roll
-    printf("Player rolled %d + %d = %d\n", die1, die2, die1 + die2);
+    printf("\nPlayer rolled %d + %d = %d\n", die1, die2, die1 + die2);
     return die1 + die2; // return sum of dice
 }
 
@@ -72,11 +72,73 @@ void craps(void) {
     }
 }
 
+void txtMenu(void) {
+    char qsOne[] = "\nWhich game would you like to play?";
+    char crapstxt[] = "Enter 1 for Craps";
+    char abysstxt[] = "Enter 2 for The Abyss";
+    char pigtxt[]   = "Enter 3 for Pig";
+    char quit[]  = "Enter 0 for Quit";
+    char choice[] = "Your choice? ";
+
+    printf("%s\n  %s\n  %s\n  %s\n%s\n%s", qsOne, crapstxt, abysstxt, pigtxt, quit, choice);
+}
+
+unsigned int chooseGame(void) {
+    unsigned int gameChoice;
+    txtMenu();
+    scanf("%u", &gameChoice);
+    if (gameChoice < 0 || gameChoice > 3)
+    {
+        fprintf(stderr, "%s", "\n---> Please, choose among options 0 to 3:\n\n");
+    }   
+    return gameChoice;
+
+}
+
+void abyss(void) {
+    /* CODE */
+
+}
+
+void pig(void) {
+    /* CODE */
+
+}
+
+void launchChoice(unsigned int gameChoice){
+    switch (gameChoice)
+    {
+
+    case 0:
+        break;
+
+    case 1:
+        craps();
+        break;
+
+    case 2:
+        abyss();
+        break;
+
+    case 3:
+        pig();
+        break;    
+
+    }
+}
+
+void menu(void) {
+    unsigned int gameChoice = rand(); // initialize gameChoice to a random variable for the while loop
+    while (gameChoice != 0) {
+        gameChoice = chooseGame(); // chooseGame filters wrong choices, if gameChoice is assigned, it must have values 0-3
+        launchChoice(gameChoice);
+    }
+}
+
 int main(void) {
     clock_t begin = clock();
     // --------- S T A R T  T O  C O D E  H E R E ---------
-    
-    craps();
+    menu();
 
     // --------- E N D  O F  C O D E
     clock_t end = clock();
